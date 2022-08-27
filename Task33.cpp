dc0ea6c4a85bfa2ced4ceeede956fb067ddd0d85
ANDREJ <and423@inbox.lv>
2022-08-27 22:55:45 +0200

    Добавьте файлы проекта.

---
+++ Task33.cpp
@@ -0,0 +1,12 @@
+﻿
+#include "Task33.h"
+
+using namespace std;
+
+int main()
+{
+  std::ofstream file("record.json");
+  file << std::setw(4) << film;
+  file.close();
+	return 0;
+}
---
+++ Task33.h
@@ -0,0 +1,17 @@
+﻿
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+nlohmann::json film{
+   {"name" , "Fall"},
+   {"country create", "USA" ,},
+   {"create date", 2022, },
+   {"creation studio", "Capstore Global" },
+   { "actors"," Grace Fulton, Virginia Gardner , Jeffrey Dean Morgan, Brianna Hildebrand,Mason Gooding, Jasper Cole" }
+
+};
